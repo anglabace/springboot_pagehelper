9da56b4cb846f6000bd204f2956865ebef962935
qfyu01 <qfyu01@mail.nfsq.com.cn>
2018-11-22 09:43:55 +0800

    setter和getter方法

--- src/main/java/com/nfsq/pagehelper/persist/dao/User.java
+++ src/main/java/com/nfsq/pagehelper/persist/dao/User.java
@@ -19,6 +19,51 @@ public class User {
     private String orgName;
     private String company;
 
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public String getOrgName() {
+        return orgName;
+    }
+
+    public void setOrgName(String orgName) {
+        this.orgName = orgName;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
 }
